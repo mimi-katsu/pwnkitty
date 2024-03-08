d5d8c585a4251852f90e3b8a5d0fc07d9863a12e
mimi-katsu <maiamor1000@gmail.com>
2024-03-07 17:48:31 -0800

    fixed some issues with newline chars in the intro ascii

--- util/intro.py
+++ util/intro.py
@@ -11,8 +11,8 @@ def play_intro():
     with open(intro_path, 'r', encoding='utf-8') as i:
         lines = i.readlines()
         for line in lines:
-            print(line)
+            print(line.strip('\n'))
             time.sleep(.1)
         
-        print('\nFrom mimi with love https://github.com/mimi-katsu/pwnkitty\n==== GNU GENERAL PUBLIC LICENSE Version 3 ====\n')
+        print('''From Maia with love :3 - https://github.com/mimi-katsu/pwnkitty\n==== GNU GENERAL PUBLIC LICENSE Version 3 ====\nPlease don't use for illegal stuff 0w0\n''')
 
